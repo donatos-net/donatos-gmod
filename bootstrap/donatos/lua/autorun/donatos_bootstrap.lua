31b645ea1fc4cd5f9bec2291318ca3fe5ef0a430
apyrr <116293161+apyrr@users.noreply.github.com>
2025-05-01 23:14:20 +0300

    formatting

--- bootstrap/donatos/lua/autorun/donatos_bootstrap.lua
+++ bootstrap/donatos/lua/autorun/donatos_bootstrap.lua
@@ -62,110 +62,110 @@ end
 ---------------------------------------------------------------------------
 
 if SERVER then
-    util.AddNetworkString("donatos_bundle")
+	util.AddNetworkString("donatos_bundle")
 
-    local cachedBundle, cachedHash
+	local cachedBundle, cachedHash
 
-    net.Receive("donatos_bundle", function (len, ply)
-    	if ply._donatosBundleRequest && CurTime() - ply._donatosBundleRequest <= 30 then
-    		return
-    	end
+	net.Receive("donatos_bundle", function (len, ply)
+		if ply._donatosBundleRequest && CurTime() - ply._donatosBundleRequest <= 30 then
+			return
+		end
 
-    	ply._donatosBundleRequest = CurTime()
+		ply._donatosBundleRequest = CurTime()
 
-        local hash = donatosBootstrap.bundleSha256ConVar:GetString()
-        if hash == "" then
-            -- аддон не запущен
-            return
-        end
+		local hash = donatosBootstrap.bundleSha256ConVar:GetString()
+		if hash == "" then
+			-- аддон не запущен
+			return
+		end
 
-        local localBundle
-        if cachedHash == hash then
-            localBundle = cachedBundle
-        else
-            localBundle = file.Read(LOCAL_BUNDLE_PATH, "DATA")
-        end
+		local localBundle
+		if cachedHash == hash then
+			localBundle = cachedBundle
+		else
+			localBundle = file.Read(LOCAL_BUNDLE_PATH, "DATA")
+		end
 
-        if !localBundle then
-            return
-        end
+		if !localBundle then
+			return
+		end
 
-        cachedBundle = localBundle
-        cachedHash = hash
+		cachedBundle = localBundle
+		cachedHash = hash
 
-        log("Игрок %s запросил bundle.lua с сервера", ply:SteamID())
+		log("Игрок %s запросил bundle.lua с сервера", ply:SteamID())
 
-        local chunkSize = 65532 - 8 - 8 - 16
-        local chunks = {}
-        for i = 1, #localBundle, chunkSize do
-            table.insert(chunks, localBundle:sub(i, i + chunkSize - 1))
-        end
+		local chunkSize = 65532 - 8 - 8 - 16
+		local chunks = {}
+		for i = 1, #localBundle, chunkSize do
+			table.insert(chunks, localBundle:sub(i, i + chunkSize - 1))
+		end
 
-        for chunkIdx, chunk in ipairs(chunks) do
-            local compressed = util.Compress(chunk)
-            net.Start("donatos_bundle")
-            net.WriteUInt(#chunks, 8)
-            net.WriteUInt(chunkIdx, 8)
-            net.WriteUInt(#compressed, 16)
-            net.WriteData(compressed, #compressed)
-            net.Send(ply)
-        end
-    end)
+		for chunkIdx, chunk in ipairs(chunks) do
+			local compressed = util.Compress(chunk)
+			net.Start("donatos_bundle")
+			net.WriteUInt(#chunks, 8)
+			net.WriteUInt(chunkIdx, 8)
+			net.WriteUInt(#compressed, 16)
+			net.WriteData(compressed, #compressed)
+			net.Send(ply)
+		end
+	end)
 end
 
 local function asyncDownloadBundleFromServer()
-    if SERVER then
-        return false, "server"
-    end
+	if SERVER then
+		return false, "server"
+	end
 
-    local running = coroutine.running()
+	local running = coroutine.running()
 
-    local resolved = false
-    local lastPacket = CurTime()
-    local function checkTimeout()
-        timer.Simple(5, function()
-            if resolved then
-                return
-            end
+	local resolved = false
+	local lastPacket = CurTime()
+	local function checkTimeout()
+		timer.Simple(5, function()
+			if resolved then
+				return
+			end
 
-            if CurTime() - lastPacket > 5 then
-                coroutine.resume(running, false, "Таймаут запроса")
-            else
-                checkTimeout()
-            end
-        end)
-    end
+			if CurTime() - lastPacket > 5 then
+				coroutine.resume(running, false, "Таймаут запроса")
+			else
+				checkTimeout()
+			end
+		end)
+	end
 
-    net.Start("donatos_bundle")
-    net.SendToServer()
+	net.Start("donatos_bundle")
+	net.SendToServer()
 
-    log("Запрос bundle.lua с сервера")
+	log("Запрос bundle.lua с сервера")
 
-    checkTimeout()
+	checkTimeout()
 
-    local chunks = {}
+	local chunks = {}
 
-    net.Receive("donatos_bundle", function (len, ply)
-        lastPacket = CurTime()
+	net.Receive("donatos_bundle", function (len, ply)
+		lastPacket = CurTime()
 
-        local totalChunks = net.ReadUInt(8)
-        local chunkIdx = net.ReadUInt(8)
-        local data = util.Decompress(net.ReadData(net.ReadUInt(16)))
+		local totalChunks = net.ReadUInt(8)
+		local chunkIdx = net.ReadUInt(8)
+		local data = util.Decompress(net.ReadData(net.ReadUInt(16)))
 
-        chunks[chunkIdx] = data
+		chunks[chunkIdx] = data
 
-        if table.Count(chunks) == totalChunks then
-            local result = ""
-            for _, key in SortedPairs(table.GetKeys(chunks)) do
-                result = result .. chunks[key]
-            end
-            resolved = true
-            log("Получен bundle.lua с сервера")
-            coroutine.resume(running, true, result)
-        end
-    end)
+		if table.Count(chunks) == totalChunks then
+			local result = ""
+			for _, key in SortedPairs(table.GetKeys(chunks)) do
+				result = result .. chunks[key]
+			end
+			resolved = true
+			log("Получен bundle.lua с сервера")
+			coroutine.resume(running, true, result)
+		end
+	end)
 
-    return coroutine.yield()
+	return coroutine.yield()
 end
 
 local function asyncFetchReleases()
@@ -210,8 +210,8 @@ local function asyncInstallRelease(release)
 
 	-- этот файл используется только на сервере
 	if SERVER then
-        file.Write(LOCAL_RELEASE_JSON_PATH, util.TableToJSON(release))
-    end
+		file.Write(LOCAL_RELEASE_JSON_PATH, util.TableToJSON(release))
+	end
 
 	log("Версия %s скачана в %s.", release.name, LOCAL_BUNDLE_PATH)
 
@@ -228,13 +228,13 @@ local function asyncBootstrap()
 
 	-- check existing bundle, server-side
 	if SERVER then
-        local localRelease = readLocalRelease()
-        if localRelease && localRelease.name && localBundle then
-            donatosBootstrap.addonVersionConVar:SetString(localRelease.name)
-            donatosBootstrap.bundleSha256ConVar:SetString(util.SHA256(localBundle))
-            return runBundle(localBundle)
-        end
-    end
+		local localRelease = readLocalRelease()
+		if localRelease && localRelease.name && localBundle then
+			donatosBootstrap.addonVersionConVar:SetString(localRelease.name)
+			donatosBootstrap.bundleSha256ConVar:SetString(util.SHA256(localBundle))
+			return runBundle(localBundle)
+		end
+	end
 
 	-- check existing bundle, client-side
 	if CLIENT && localBundle then
@@ -252,11 +252,11 @@ local function asyncBootstrap()
 	end
 
 	if SERVER then
-        local remoteReleasesSuccess, remoteReleases = asyncFetchReleases()
-        if !remoteReleasesSuccess then
-            log("Не удалось получить список релизов аддона.")
-            return false
-        end
+		local remoteReleasesSuccess, remoteReleases = asyncFetchReleases()
+		if !remoteReleasesSuccess then
+			log("Не удалось получить список релизов аддона.")
+			return false
+		end
 
 		local latestRelease = remoteReleases.releases[1]
 		if !latestRelease then
@@ -272,55 +272,55 @@ local function asyncBootstrap()
 		donatosBootstrap.bundleSha256ConVar:SetString(util.SHA256(bundle))
 		return runBundle(bundle)
 	else
-        local function clientHttpBootstrap()
-            local version = donatosBootstrap.addonVersionConVar:GetString()
-            if version == "" then
-                log("Сервер не передал %s", donatosBootstrap.addonVersionConVar:GetName())
-                return false
-            end
+		local function clientHttpBootstrap()
+			local version = donatosBootstrap.addonVersionConVar:GetString()
+			if version == "" then
+				log("Сервер не передал %s", donatosBootstrap.addonVersionConVar:GetName())
+				return false
+			end
 
-            local remoteReleasesSuccess, remoteReleases = asyncFetchReleases()
-            if !remoteReleasesSuccess then
-                log("Не удалось получить список релизов аддона.")
-                return false
-            end
+			local remoteReleasesSuccess, remoteReleases = asyncFetchReleases()
+			if !remoteReleasesSuccess then
+				log("Не удалось получить список релизов аддона.")
+				return false
+			end
 
-            local release
-            for _, r in ipairs(remoteReleases.releases) do
-                if r.name == version then
-                    release = r
-                    break
-                end
-            end
+			local release
+			for _, r in ipairs(remoteReleases.releases) do
+				if r.name == version then
+					release = r
+					break
+				end
+			end
 
-            if !release then
-                log("Релиз %s недоступен.", version)
-                return false
-            end
+			if !release then
+				log("Релиз %s недоступен.", version)
+				return false
+			end
 
-            local success, bundle = asyncInstallRelease(release)
-            if !success then
-                log(bundle) -- err
-                return false
-            end
-            return runBundle(bundle)
-        end
+			local success, bundle = asyncInstallRelease(release)
+			if !success then
+				log(bundle) -- err
+				return false
+			end
+			return runBundle(bundle)
+		end
 
-        local success, result = clientHttpBootstrap()
-        if success then
-            return result
-        end
+		local success, result = clientHttpBootstrap()
+		if success then
+			return result
+		end
 
-        -- если не получилось скачать аддон через http, запрашиваем с сервера через net
-        local success, bundle = asyncDownloadBundleFromServer()
-        if success then
-            file.CreateDir("donatos")
-            file.Write(LOCAL_BUNDLE_PATH, bundle)
-            return runBundle(bundle)
-        else
-            log("Не удалось получить bundle.lua с сервера: %s", bundle)
-            return false
-        end
+		-- если не получилось скачать аддон через http, запрашиваем с сервера через net
+		local success, bundle = asyncDownloadBundleFromServer()
+		if success then
+			file.CreateDir("donatos")
+			file.Write(LOCAL_BUNDLE_PATH, bundle)
+			return runBundle(bundle)
+		else
+			log("Не удалось получить bundle.lua с сервера: %s", bundle)
+			return false
+		end
 	end
 end
 
